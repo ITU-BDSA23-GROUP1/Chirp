18933be67753c5d108d00c5093f223d2bebfd837
Alex Bjørnskov <alebj@itu.dk>
2023-09-07 14:57:42 +0200

    Refactor read and write to be in methods for readability
    Changed variable names to fit Code Standard
    
    Changed class Cheep to record cheep for later use
    
    Co-authored-by: Mathilde <mgon@itu.dk>
    Co-authored-by: Carmen <alcn@itu.dk>
    Co-authored-by: Olivia <olmo@itu.dk>
    Co-authored-by: Emilie <emru@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -15,30 +15,43 @@ try
 {
     if (args[0] == "read")
     {
-        using (var sr = new StreamReader("chirp_cli_db.csv"))
-        {
-            string line; 
-            sr.ReadLine();
-            while ((line = sr.ReadLine()) != null)
-            {
-                //Define pattern
-                Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-
-                //Separating columns to array
-                string[] cheep = CSVParser.Split(line);
-                // Following lines inspired by https://peterdaugaardrasmussen.com/2022/11/26/csharp-convert-datetimeoffset-to-and-from-unix-timestamp/#:~:text=In%20order%20to%20get%20a%20DateTime%20form%20a,DateTimeOffset.FromUnixTimeMilliseconds%281669321628392%29%3B%20var%20dateTime%20%3D%20dateTimeOffset.DateTime%3B%20That%20is%20all%21
-                DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(cheep[2]));
-                // Following lines inspired by  https://learn.microsoft.com/en-us/dotnet/api/system.datetime.tolocaltime?view=net-7.0
-                var localTime = time.DateTime.ToLocalTime();
-                Console.WriteLine($"{cheep[0]} @ {localTime.ToString("MM/dd/yy HH:mm:ss")}: {cheep[1].Substring(1, cheep[1].Length - 2)}");
-            }
-        }
-
+        Read();
     } else if (args[0] == "cheep") 
     {
-        // Next couple of lines are inspired by https://joshclose.github.io/CsvHelper/examples/writing/appending-to-an-existing-file/
+        Write(args[1]);
+    } 
+}
+catch (IOException E)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(E.Message);
+}
+
+static void Read() {
+    using (var Sr = new StreamReader("chirp_cli_db.csv"))
+    {
+        string? Line; 
+        Sr.ReadLine();
+        while ((Line = Sr.ReadLine()) != null)
+        {
+            //Define pattern
+            Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+            //Separating columns to array
+            string[] Cheep = CSVParser.Split(Line);
+            // Following lines inspired by https://peterdaugaardrasmussen.com/2022/11/26/csharp-convert-datetimeoffset-to-and-from-unix-timestamp/#:~:text=In%20order%20to%20get%20a%20DateTime%20form%20a,DateTimeOffset.FromUnixTimeMilliseconds%281669321628392%29%3B%20var%20dateTime%20%3D%20dateTimeOffset.DateTime%3B%20That%20is%20all%21
+            DateTimeOffset Time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(Cheep[2]));
+            // Following lines inspired by  https://learn.microsoft.com/en-us/dotnet/api/system.datetime.tolocaltime?view=net-7.0
+            var LocalTime = Time.DateTime.ToLocalTime();
+            Console.WriteLine($"{Cheep[0]} @ {LocalTime.ToString("MM/dd/yy HH:mm:ss")}: {Cheep[1].Substring(1, Cheep[1].Length - 2)}");
+        }
+    }
+}
+
+
+static void Write(string CheepMsg) {
+    // Next couple of lines are inspired by https://joshclose.github.io/CsvHelper/examples/writing/appending-to-an-existing-file/
         // Append to the file.
-        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        var Config = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
             // Don't write the header again.
             HasHeaderRecord = false,
@@ -52,27 +65,21 @@ try
             }
         };
     
-        using (var stream = File.Open("chirp_cli_db.csv", FileMode.Append))
-        using (var writer = new StreamWriter(stream))
-        using (var csv = new CsvWriter(writer, config))
+        using (var Stream = File.Open("chirp_cli_db.csv", FileMode.Append))
+        using (var Writer = new StreamWriter(Stream))
+        using (var Csv = new CsvWriter(Writer, Config))
         {
-            DateTime currentTime = DateTime.Now;
-            long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
+            DateTime CurrentTime = DateTime.Now;
+            long UnixTime = ((DateTimeOffset)CurrentTime).ToUnixTimeSeconds();
 
-            csv.NextRecord();
+            Csv.NextRecord();
             //Following line inspired by https://learn.microsoft.com/en-us/dotnet/api/system.environment.username?view=net-7.0
-            csv.WriteRecord(new Cheep {author = Environment.UserName, message = args[1], timestamp = unixTime});
+            Csv.WriteRecord(new Cheep {Author = Environment.UserName, Message = CheepMsg, Timestamp = UnixTime});
         }
-    } 
-}
-catch (IOException e)
-{
-    Console.WriteLine("The file could not be read:");
-    Console.WriteLine(e.Message);
 }
 
-public class Cheep {
-    public string author { get; set; } = null!;
-    public string message { get; set; } = null!;
-    public long timestamp { get; set; }
+public record Cheep {
+    public string Author { get; set; } = null!;
+    public string Message { get; set; } = null!;
+    public long Timestamp { get; set; }
 }

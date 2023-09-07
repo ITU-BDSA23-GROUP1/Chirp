f75826ed1b212d39a11b89696453f43e778e5622
Alex Bjørnskov <alebj@itu.dk>
2023-09-07 17:10:55 +0200

    Refactored data persistence to library project SimpleDB
    To avoid code redundancy and enhance extensibility this change was made.
    
    We now read and write from csv files only through the CSVDatabase class
    
    Program.cs calls that database to get data from the appropriate csvfile
    
    Co-authored-by: Olivia <olmo@itu.dk>
    Co-authored-by: Carmen <alcn@itu.dk>
    Co-authored-by: Emilie <emru@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -4,12 +4,16 @@ using System.Text.RegularExpressions;
 using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
+using SimpleDB;
 
 // Reading the CSV file is inspired by:
 // - https://stackoverflow.com/questions/3507498/reading-csv-files-using-c-sharp/34265869#34265869
 // - https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-read-text-from-a-file
 
 // Open the text file using a stream reader.
+
+IDatabaseRepository<Cheep> cd = new CSVDatabase<Cheep>();
+cd.fileName = "../Chirp.CLI/chirp_cli_db.csv";
 
 try
 {
@@ -27,52 +31,26 @@ catch (IOException E)
     Console.WriteLine(E.Message);
 }
 
-static void Read() {
-    using (var Sr = new StreamReader("chirp_cli_db.csv"))
-    using (var Csv = new CsvReader(Sr, CultureInfo.InvariantCulture))
+void Read() {
+    IEnumerable<Cheep> Records = cd.Read(10);
+    foreach (Cheep CurCheep in Records)
     {
-        var Records = Csv.GetRecords<Cheep>();
-        foreach (Cheep CurCheep in Records)
-        {
-            // Following lines inspired by https://peterdaugaardrasmussen.com/2022/11/26/csharp-convert-datetimeoffset-to-and-from-unix-timestamp/#:~:text=In%20order%20to%20get%20a%20DateTime%20form%20a,DateTimeOffset.FromUnixTimeMilliseconds%281669321628392%29%3B%20var%20dateTime%20%3D%20dateTimeOffset.DateTime%3B%20That%20is%20all%21
-            DateTimeOffset Time = DateTimeOffset.FromUnixTimeSeconds(CurCheep.Timestamp);
-            // Following lines inspired by  https://learn.microsoft.com/en-us/dotnet/api/system.datetime.tolocaltime?view=net-7.0
-            var LocalTime = Time.DateTime.ToLocalTime();
-            Console.WriteLine($"{CurCheep.Author} @ {LocalTime}: {CurCheep.Message}");
-
-        }
+        // Following lines inspired by https://peterdaugaardrasmussen.com/2022/11/26/csharp-convert-datetimeoffset-to-and-from-unix-timestamp/#:~:text=In%20order%20to%20get%20a%20DateTime%20form%20a,DateTimeOffset.FromUnixTimeMilliseconds%281669321628392%29%3B%20var%20dateTime%20%3D%20dateTimeOffset.DateTime%3B%20That%20is%20all%21
+        DateTimeOffset Time = DateTimeOffset.FromUnixTimeSeconds(CurCheep.Timestamp);
+        // Following lines inspired by  https://learn.microsoft.com/en-us/dotnet/api/system.datetime.tolocaltime?view=net-7.0
+        var LocalTime = Time.DateTime.ToLocalTime();
+        Console.WriteLine($"{CurCheep.Author} @ {LocalTime}: {CurCheep.Message}");
     }
 }
 
 
-static void Write(string CheepMsg) {
-    // Next couple of lines are inspired by https://joshclose.github.io/CsvHelper/examples/writing/appending-to-an-existing-file/
-        // Append to the file.
-        var Config = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            // Don't write the header again.
-            HasHeaderRecord = false,
-            // Following lines are inspired by https://stackoverflow.com/questions/56579848/csvhelper-configuration-shouldquote-return-true-for-only-fields-on-the-dto-whi
-            ShouldQuote = args =>
-            {
-                if (string.IsNullOrEmpty(args.Field)) return false;
-                else if (args.Field.Equals(Environment.UserName)) return false;
-
-                return args.FieldType == typeof(string);
-            }
-        };
+void Write(string CheepMsg) {
+    DateTime CurrentTime = DateTime.Now;
+    long UnixTime = ((DateTimeOffset)CurrentTime).ToUnixTimeSeconds();
     
-        using (var Stream = File.Open("chirp_cli_db.csv", FileMode.Append))
-        using (var Writer = new StreamWriter(Stream))
-        using (var Csv = new CsvWriter(Writer, Config))
-        {
-            DateTime CurrentTime = DateTime.Now;
-            long UnixTime = ((DateTimeOffset)CurrentTime).ToUnixTimeSeconds();
-
-            Csv.NextRecord();
-            //Following line inspired by https://learn.microsoft.com/en-us/dotnet/api/system.environment.username?view=net-7.0
-            Csv.WriteRecord(new Cheep {Author = Environment.UserName, Message = CheepMsg, Timestamp = UnixTime});
-        }
+    Cheep cheep = new Cheep {Author = Environment.UserName, Message = {CheepMsg}, Timestamp = UnixTime};
+    
+    cd.Store(cheep);
 }
 
 public record Cheep {

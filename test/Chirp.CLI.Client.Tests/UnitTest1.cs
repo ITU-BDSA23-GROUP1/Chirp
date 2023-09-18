d219db42a2e32a49b38213d92bdc1f1ba7ebd38b
Emilie <emru@itu.dk>
2023-09-18 15:58:33 +0200

    Make first attemt on unit test for unix time, doesn't work yet
    Co-authored-by: Mathilde <mgon@itu.dk>
    Co-authored-by: Carmen <alcn@itu.dk>
    Co-authored-by: Olivia <olmo@itu.dk>
    Co-authored-by: Alex <alebj@itu.dk>

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -1,10 +1,15 @@
-namespace Chirp.CLI.Client.Tests;
+using Chirp.CLI;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void getUnixTimeTest()
     {
+        DateTime dateTime = new DateTime(2023, 9, 10, 12, 30, 0);
+
+        long expectedUnixTime = 1694349000;
+
+        Assert.Equal(expectedUnixTime, getUnixTime(dateTime));
 
     }
 }

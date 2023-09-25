09dfc1003e8b3d1359f13946d641177049ed59fa
Alex Bjørnskov <alebj@itu.dk>
2023-09-25 17:04:54 +0200

    Add CreateFile and DeleteFile to integrationTests such that we create a csv file to test and delete it after the test
    This is done to assure that different tests doesn't access the same file
    
    Co-authored-by: Carmen <alcn@itu.dk>
    Co-authored-by: Olivia <olmo@itu.dk>

--- test/Chirp.SimpleDB.Tests/IntegrationTest.cs
+++ test/Chirp.SimpleDB.Tests/IntegrationTest.cs
@@ -1,21 +1,34 @@
-using System.ComponentModel.Design;
 using SimpleDB;
-using FluentAssertions;
-
 
 namespace Chirp.SimpleDB.Tests;
 
 public class IntegrationTest
 {
+
+    private static void CreateFile()
+    {
+        if (File.Exists("../../../test_chirp.csv"))
+        {
+            File.Delete("../../../test_chirp.csv");
+        }
+        File.WriteAllText("../../../test_chirp.csv", "Author,Message,Timestamp");
+    }
+
+    private static void DeleteFile()
+    {
+        File.Delete("../../../test_chirp.csv");
+    }
+
+
     [Fact]
     public void StoreAndRead_EntryFromDataBase_CorrectCheep()
     {
         // Arrange
-
+        CreateFile();
         CSVDatabase<Cheep> testDB = CSVDatabase<Cheep>.Instance;
         testDB.fileName = "../../../test_chirp.csv";
 
-        Cheep testCheep = new Cheep { Author = "Username", Message = "Test123", Timestamp = 1694349000 };
+        Cheep testCheep = new Cheep { Author = "Username", Message = "\"Test123\"", Timestamp = 1694349000 };
 
         // Act
         testDB.Store(testCheep);
@@ -23,8 +36,10 @@ public class IntegrationTest
         Cheep storedCheep = cheeps.First();
 
         // Assert
-
-        Assert.Equal(testCheep,storedCheep);
+        Assert.Equal(testCheep.Author, storedCheep.Author);
+        Assert.Equal(testCheep.Message, $"\"{storedCheep.Message}\"");
+        Assert.Equal(testCheep.Timestamp, storedCheep.Timestamp);
+        DeleteFile();
 
     }
 }

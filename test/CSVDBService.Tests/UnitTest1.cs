e7342cf9cc0732f9826c6fb9c291f2b1405a0051
Olivia <118437117+olivia2300@users.noreply.github.com>
2023-09-26 15:10:29 +0200

    Create test for get cheeps status code. Co-authored-by: Carmen <alcn@itu.dk> Co-authored-by: Emilie <emru@itu.dk>

--- test/CSVDBService.Tests/UnitTest1.cs
+++ test/CSVDBService.Tests/UnitTest1.cs
@@ -1,10 +1,37 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
 namespace CSVDBService.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void GetCheepsTest()
+    public async Task GetCheepsTest()
     {
+        //Arrange
+        var baseURL = "http://localhost:5000";
+        using HttpClient client = new();
+
+        client.BaseAddress = new Uri(baseURL);
+
+
+        //Act
+        var response = await client.GetAsync("/cheeps");
+
+
+        //Assert
+
+
+        //Assert.IsType<List<Cheep>>(response.Content.);
+        //Assert.IsType<JsonContent>(response.Content);
+        //Assert.IsType<List<Cheep>>(JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()));
+
+
+        Assert.Equal(HttpStatusCode.OK,  response.StatusCode);
+
+
 
     }
 }

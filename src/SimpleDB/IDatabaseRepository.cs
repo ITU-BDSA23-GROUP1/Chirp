68fd1eafca3addfdf9076cce36f613eb2c459951
Olivia <118437117+olivia2300@users.noreply.github.com>
2023-09-28 16:22:13 +0200

    remove csv files and create razor page
    Co-authored-by: Emilie <emru@itu.dk>
    Co-authored-by: Alex <alebj@itu.dk>
    Co-authored-by: Carmen <alcn@itu.dk>

--- src/SimpleDB/IDatabaseRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace SimpleDB;
-
-public interface IDatabaseRepository<T>{
-
-    public string fileName { get; set; }
-    public IEnumerable<T> Read(int? limit = null);
-    public void Store(T record);
-}

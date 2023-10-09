2be5fa11597ad36e65c048037bf1718b3002ebf1
Alex Bjørnskov <alebj@itu.dk>
2023-10-09 14:49:09 +0200

    Delete IRepo.cs and added the corresponding interface into the CheepRepo.cs.
    Co-authored-by: Mathilde <mgon@itu.dk>
    Co-authored-by: Carmen <alcn@itu.dk>
    Co-authored-by: Olivia <olmo@itu.dk>
    Co-authored-by: Emilie <emru@itu.dk>

--- Repository/CheepRepo.cs
+++ Repository/CheepRepo.cs
@@ -1,43 +1,51 @@
 using Chirp.EF;
 
+public interface IRepository<out T, in TFilter>
+{
+    public IEnumerable<T> Get(int offset);
+    public IEnumerable<T> GetByFilter(TFilter attribute, int offset);
+}
+
 public class CheepRepository : IRepository<Cheep, Author>
 {
 
     private readonly CheepContext context;
 
-     public CheepRepository(OrderingContext context)
-        {
-            context = context ?? throw new ArgumentNullException(nameof(context));
-        }
+    public CheepRepository(OrderingContext context)
+    {
+        context = context ?? throw new ArgumentNullException(nameof(context));
+    }
 
-     public Cheep Add(Cheep cheep)
-        {
-            return context.cheeps.Add(cheep).Entity;
-        }
+    public Cheep Add(Cheep cheep)
+    {
+        return context.cheeps.Add(cheep).Entity;
+    }
 
     public async Task<Cheep> GetCheepsAsync(int offset)
-        {
-            var cheeps = await
-                .From c in context.cheeps
-                .limit(32).offset(offset)
-                .Select(new Cheep(author: new Author(authorID: c.author.authorID, name: c.author.name, email: c.author.email), text: c.text, timeStamp: c.timeStamp))
-                .ToListAsync()
+    {
+        var cheeps = await
+
+            .From c in context.cheeps
+            .limit(32).offset(offset)
+            .Select(new Cheep(author: new Author(authorID: c.author.authorID, name: c.author.name, email: c.author.email), text: c.text, timeStamp: c.timeStamp))
+            .ToListAsync()
 
             return cheeps;
-        }
+    }
 
 
-     public async Task<Cheep> GetCheepsByAuthorAsync(string authorID, int offset)
-        {
-            var cheeps = await
-                .From c in context.cheeps
-                .limit(32).offset(offset)
-                .Where (c => c.authorID == authorID)
-                .Select(new Cheep(author: new Author(authorID: c.author.authorID, name: c.author.name, email: c.author.email), text: c.text, timeStamp: c.timeStamp))
-                .ToListAsync()
+    public async Task<Cheep> GetCheepsByAuthorAsync(string authorID, int offset)
+    {
+        var cheeps = await
+
+            .From c in context.cheeps
+            .limit(32).offset(offset)
+            .Where(c => c.authorID == authorID)
+            .Select(new Cheep(author: new Author(authorID: c.author.authorID, name: c.author.name, email: c.author.email), text: c.text, timeStamp: c.timeStamp))
+            .ToListAsync()
 
             return cheeps;
-        }
+    }
 
 
 }
--- Repository/IRepo.cs
+++
@@ -1,7 +0,0 @@
-namespace Repositories;
-
-public interface IRepository<out T, in TFilter>
-{
-    public IEnumerable<T> Get(int offset);
-    public IEnumerable<T> GetByFilter(TFilter attribute, int offset);
-}

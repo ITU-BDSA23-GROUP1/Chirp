7d058519472343c90935584df5cdd7356d25b323
Olivia <118437117+olivia2300@users.noreply.github.com>
2023-10-16 11:54:21 +0200

    Extend repo and Interface repo with new methods
    Co-authored-by: Emilie <emru@itu.dk>
    Co-authored-by: Alex <alebj@itu.dk>

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -4,4 +4,8 @@ public interface ICheepRepository<T, in Tstring>
 {
     public Task<IEnumerable<T>> Get(int offset);
     public Task<IEnumerable<T>> GetByFilter(string attribute, int offset);
+    public Task<AuthorDTO> FindAuthorByName(string name);
+    public Task<AuthorDTO> FindAuthorByEmail(string email);
+    public AuthorDTO CreateAuthor(int authorID, string name); 
+    public CheepDTO CreateCheep(string text, DateTime timeStamp, AuthorDTO author);
 }
--- src/Chirp.Infrastructure/CheepRepo.cs
+++ src/Chirp.Infrastructure/CheepRepo.cs
@@ -61,4 +61,63 @@ public class CheepRepository : ICheepRepository<CheepDTO, string>
     }
 
 
+    public async Task<AuthorDTO> FindAuthorByName(string name) 
+    {
+        var author = await context.Authors
+            .Where(a => a.Name == name)
+            .Select(a => new AuthorDTO
+            {
+                authorID = a.AuthorId,
+                name = a.Name,
+            })
+            .FirstOrDefaultAsync(); //Maybe delete this line
+
+        return author;
+    }
+
+
+    public async Task<AuthorDTO> FindAuthorByEmail(string email)
+    {
+        var author = await context.Authors
+            .Where(a => a.Email == email)
+            .Select(a => new AuthorDTO
+            {
+                authorID = a.AuthorId,
+                name = a.Name,
+            })
+            .FirstOrDefaultAsync(); //Maybe delete this line
+
+        return author;
+    }
+
+    public AuthorDTO CreateAuthor(int authorID, string name)
+    {
+        var author = new AuthorDTO
+        {
+            authorID = authorID,
+            name = name,     
+            };
+
+        return author;
+    }
+
+    public CheepDTO CreateCheep(string text, DateTime timeStamp, AuthorDTO author)
+    {
+        AuthorDTO checkAuthor = FindAuthorByName(author.name).Result;
+        if (FindAuthorByName(author.name) == null)
+        {
+            checkAuthor = CreateAuthor(author.authorID, author.name); 
+        }
+
+        var cheep = new CheepDTO
+        {
+            text = text,
+            timeStamp = timeStamp,
+            author = checkAuthor
+        };
+
+        return cheep;
+    }
+
+
 }

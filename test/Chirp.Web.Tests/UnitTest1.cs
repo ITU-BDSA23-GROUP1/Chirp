fa1375159209889e64779bc383a188270a056ddb
Olivia <118437117+olivia2300@users.noreply.github.com>
2023-10-16 13:48:34 +0200

    create test files for Core, Web and Infrastructure
    Co-authored-by: Emilie <emru@itu.dk>
    Co-authored-by: Alex <alebj@itu.dk>

---
+++ test/Chirp.Web.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+//using Chirp.Infrastructure;
+using Chirp.Web;
+
+namespace Chirp.Web.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AddCheep_CheckCheepEntity()
+    {
+        //Arrange
+       
+
+        //Act
+
+
+        //Assert
+
+    }
+}

d466c712e64225442cd1d71561aa40aac7341fa5
Carmen Alberte Nielsen <alcn@itu.dk>
2023-12-07 17:33:32 +0100

    Initial playwright install Not working yet
    Co-authored-by: Alex <alebj@itu.dk>

---
+++ test/PlaywrightTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PlaywrightTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

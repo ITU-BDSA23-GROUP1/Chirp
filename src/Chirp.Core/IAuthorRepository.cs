cdfc902066917f59d94ecf64e72ad1bb83cf3e44
Alex Bjørnskov <alebj@itu.dk>
2023-12-17 18:15:28 +0100

    Delete CreateAuthor from IAuthorRepository

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -10,7 +10,6 @@ public interface IAuthorRepository<T, in Tstring>
 {
     public Task<AuthorDTO> FindAuthorByName(string name);
     public Task<AuthorDTO> FindAuthorByEmail(string email);
-    public Task CreateAuthor(AuthorDTO authorDTO);
     public Task FollowAuthor(AuthorDTO authorDTO, AuthorDTO authorToFollowDTO);
     public Task UnfollowAuthor(AuthorDTO authorDTO, AuthorDTO authorToUnfollowDTO);
     public Task<IEnumerable<string>> GetFollowing(AuthorDTO authorDTO);

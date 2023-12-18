1da9b9510060d080f1de3de4b4a30577835a2396
Emilie <emru@itu.dk>
2023-12-18 17:24:12 +0100

    made global usings in test project

---
+++ test/PlaywrightTest/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using NUnit.Framework;
+global using System.Text.RegularExpressions;
+global using System.Threading.Tasks;
+global using Microsoft.Playwright;
+global using Microsoft.Playwright.NUnit;
--- test/PlaywrightTest/UITest.cs
+++ test/PlaywrightTest/UITest.cs
@@ -1,10 +1,3 @@
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.Playwright;
-using Microsoft.Playwright.NUnit;
-using NUnit.Framework;
-
-
 namespace PlaywrightTest;
 
 [Parallelizable(ParallelScope.Self)]
